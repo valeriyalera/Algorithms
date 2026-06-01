de5ad391ec96167c157ca47a1a32775470ff4dc7
valeriyalera <valeriyalera2007@gmail.com>
2026-06-02 01:34:49 +0300

    Update 1. Two Sum.cpp

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -1,1 +1,21 @@
+#include <unordered_map>
+#include <vector>
 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::unordered_map<int, int> numMap;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            
+            if (numMap.find(complement) != numMap.end()) {
+                return {numMap[complement], i};
+            }
+            
+            numMap[nums[i]] = i;
+        }
+        
+        return {};
+    }
+};
